d83c17010844dac8d753400fdb265101742d9231
Robert Klim <kontakt.robert.klim@gmail.com>
2018-08-12 09:35:51 +0200

    Add 'Recipe' component

---
+++ recipes/components/Recipe.vue
@@ -0,0 +1,28 @@
+<template>
+    <article class="recipe">
+        <div class="thumbnail" :style="{backgroundImage: 'url(' + thumbnail + category + ')'}"></div>
+        <h3>{{ title }}</h3>
+        <p>{{ previewText }}</p>
+    </article>
+</template>
+
+<script>
+export default {
+    props: ['thumbnail', 'title', 'previewText', 'category']
+}
+</script>
+<style scoped>
+    .recipe {
+        box-sizing: border-box;
+        width: 300px;
+        padding: 10px;
+        margin: 5px;
+        border: 1px solid #ccc;
+        box-shadow: 0 2px 2px #aaa;
+    }
+    .thumbnail {
+        background-size: cover;
+        width: 100%;
+        height: 200px;
+    }
+</style>
--- recipes/pages/recipes/index.vue
+++ recipes/pages/recipes/index.vue
@@ -1,37 +1,52 @@
 <template>
     <section class="recipes">
-        <article class="recipe">
-            <div class="thumbnail" style="background-image: url('https://source.unsplash.com/1600x900/?pizza');"></div>
-            <h3>Recipe 1</h3>
-            <p>Some nuxt level preview text...</p>
-        </article>
-        <article class="recipe">
-            <div class="thumbnail" style="background-image: url('https://source.unsplash.com/1600x900/?food');"></div>
-            <h3>Recipe 2</h3>
-            <p>Some nuxt level preview text...</p>
-        </article>
-        <article class="recipe">
-            <div class="thumbnail" style="background-image: url('https://source.unsplash.com/1600x900/?pasta');"></div>
-            <h3>Recipe 3</h3>
-            <p>Some nuxt level preview text...</p>
-        </article>
-        <article class="recipe">
-            <div class="thumbnail" style="background-image: url('https://source.unsplash.com/1600x900/?burger');"></div>
-            <h3>Recipe 4</h3>
-            <p>Some nuxt level preview text...</p>
-        </article>
-        <article class="recipe">
-            <div class="thumbnail" style="background-image: url('https://source.unsplash.com/1600x900/?steak');"></div>
-            <h3>Recipe 5</h3>
-            <p>Some nuxt level preview text...</p>
-        </article>
-        <article class="recipe">
-            <div class="thumbnail" style="background-image: url('https://source.unsplash.com/1600x900/?salad');"></div>
-            <h3>Recipe 5</h3>
-            <p>Some nuxt level preview text...</p>
-        </article>
+        <Recipe 
+            thumbnail="https://source.unsplash.com/1600x900/?"
+            title="Recipe 1"
+            previewText="Some nuxt level preview text..."
+            category="pizza"
+        />
+        <Recipe 
+            thumbnail="https://source.unsplash.com/1600x900/?"
+            title="Recipe 2"
+            previewText="Some nuxt level preview text..."
+            category="food"
+        />
+        <Recipe 
+            thumbnail="https://source.unsplash.com/1600x900/?"
+            title="Recipe 3"
+            previewText="Some nuxt level preview text..."
+            category="drink"
+        />
+        <Recipe 
+            thumbnail="https://source.unsplash.com/1600x900/?"
+            title="Recipe 4"
+            previewText="Some nuxt level preview text..."
+            category="burger"
+        />
+        <Recipe 
+            thumbnail="https://source.unsplash.com/1600x900/?"
+            title="Recipe 5"
+            previewText="Some nuxt level preview text..."
+            category="taco"
+        />
+        <Recipe 
+            thumbnail="https://source.unsplash.com/1600x900/?"
+            title="Recipe 6"
+            previewText="Some nuxt level preview text..."
+            category="cake"
+        />
     </section>
 </template>
+
+<script>
+import Recipe from '@/components/Recipe';
+export default {
+    components: {
+        Recipe,
+    },
+}
+</script>
 
 <style scoped>
     .recipes {
@@ -39,18 +54,5 @@
         flex-flow: row wrap;
         justify-content: center;
         align-items: center;
-    }
-    .recipe {
-        box-sizing: border-box;
-        width: 300px;
-        padding: 10px;
-        margin: 5px;
-        border: 1px solid #ccc;
-        box-shadow: 0 2px 2px #aaa;
-    }
-    .thumbnail {
-        background-size: cover;
-        width: 100%;
-        height: 200px;
     }
 </style>
